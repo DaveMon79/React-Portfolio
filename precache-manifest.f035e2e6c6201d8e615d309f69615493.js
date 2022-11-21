30591d323f49af1630b1e5f093c9daa16678b62b
DMon <davemonaghan@gmx.com>
2022-11-21 21:45:58 +0000

    Updates

---
+++ precache-manifest.f035e2e6c6201d8e615d309f69615493.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c58da1cbc4ecb7080178",
+    "url": "/React-Portfolio/static/css/main.a2926b65.chunk.css"
+  },
+  {
+    "revision": "c58da1cbc4ecb7080178",
+    "url": "/React-Portfolio/static/js/main.c58da1cb.chunk.js"
+  },
+  {
+    "revision": "f427326fb32135608909",
+    "url": "/React-Portfolio/static/js/1.f427326f.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "12746e2546e49b39517872e09b9abb28",
+    "url": "/React-Portfolio/static/media/profile-pic.12746e25.png"
+  },
+  {
+    "revision": "c3a4d8138bfcc3b797aa456b7bda6770",
+    "url": "/React-Portfolio/static/media/tradr.c3a4d813.png"
+  },
+  {
+    "revision": "e02e02ef2fcf914f7d6f7580758fc561",
+    "url": "/React-Portfolio/static/media/geek-blog.e02e02ef.png"
+  },
+  {
+    "revision": "676201e42b2e74ce9b92ba44178010d2",
+    "url": "/React-Portfolio/static/media/weather.676201e4.png"
+  },
+  {
+    "revision": "44e45f47dbef61df4d077d4ef6770f92",
+    "url": "/React-Portfolio/static/media/movie-madness.44e45f47.png"
+  },
+  {
+    "revision": "8c81c2545f123d11e327af55ab8db550",
+    "url": "/React-Portfolio/static/media/mongo.8c81c254.png"
+  },
+  {
+    "revision": "304ec737e97e5765636f151d1bfa90d5",
+    "url": "/React-Portfolio/static/media/geekquiz.304ec737.png"
+  },
+  {
+    "revision": "c8e7570782dabc8d32299a03eea35a3c",
+    "url": "/React-Portfolio/static/media/cv.c8e75707.pdf"
+  },
+  {
+    "revision": "3ba96b9434ea200edd74cca120dfddfe",
+    "url": "/React-Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63eda9caf20affe82a151fdc4a53a963.js"
+  "/React-Portfolio/precache-manifest.f035e2e6c6201d8e615d309f69615493.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
