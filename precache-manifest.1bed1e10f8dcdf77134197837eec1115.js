48e3194c5d755fe3901481f93889a85a40690b07
DMon <davemonaghan@gmx.com>
2022-11-21 14:06:01 +0000

    Updates

---
+++ precache-manifest.1bed1e10f8dcdf77134197837eec1115.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e53aafae32540fa3fd13",
+    "url": "/static/css/main.a4024b42.chunk.css"
+  },
+  {
+    "revision": "e53aafae32540fa3fd13",
+    "url": "/static/js/main.e53aafae.chunk.js"
+  },
+  {
+    "revision": "f427326fb32135608909",
+    "url": "/static/js/1.f427326f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12746e2546e49b39517872e09b9abb28",
+    "url": "/static/media/profile-pic.12746e25.png"
+  },
+  {
+    "revision": "c3a4d8138bfcc3b797aa456b7bda6770",
+    "url": "/static/media/tradr.c3a4d813.png"
+  },
+  {
+    "revision": "e02e02ef2fcf914f7d6f7580758fc561",
+    "url": "/static/media/geek-blog.e02e02ef.png"
+  },
+  {
+    "revision": "676201e42b2e74ce9b92ba44178010d2",
+    "url": "/static/media/weather.676201e4.png"
+  },
+  {
+    "revision": "44e45f47dbef61df4d077d4ef6770f92",
+    "url": "/static/media/movie-madness.44e45f47.png"
+  },
+  {
+    "revision": "8c81c2545f123d11e327af55ab8db550",
+    "url": "/static/media/mongo.8c81c254.png"
+  },
+  {
+    "revision": "304ec737e97e5765636f151d1bfa90d5",
+    "url": "/static/media/geekquiz.304ec737.png"
+  },
+  {
+    "revision": "c8e7570782dabc8d32299a03eea35a3c",
+    "url": "/static/media/cv.c8e75707.pdf"
+  },
+  {
+    "revision": "664822012e612b73ebed1754b62b30ba",
+    "url": "/index.html"
+  }
+];
