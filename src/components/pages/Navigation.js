1eb79aa067e8f98f934a3ae5d3cf6eec0b9edbac
DMon <davemonaghan@gmx.com>
2022-11-22 21:10:14 +0000

    more navbar fixes

--- src/components/pages/Navigation.js
+++ src/components/pages/Navigation.js
@@ -7,7 +7,7 @@ function Navigation({ currentPage, handlePageChange }) {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <div  id="navbarNavAltMarkup">
-          <div className="navbar-nav">
+        <div className='navbar-nav' style={{ display: 'flex', flexDirection: 'row', columnGap: '1rem' }}>
             <a className="nav-link active" aria-current="page" href="#About" onClick={() => handlePageChange('About')}>About</a>
             <a className="nav-link" href="#Project" onClick={() => handlePageChange('Project')}>Projects</a>
             <a className="nav-link" href="#Contact" onClick={() => handlePageChange('Contact')}>Contact</a>
